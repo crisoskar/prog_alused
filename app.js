8f2dba23959d913a2b82fc5637421abc6f4cfdb3
Cris <c.orav@khk.ee>
2021-10-14 09:42:29 +0300

    Loodud projekti .gitignore fail Teksti väljastamine konsooli ja kommenteerimine Muutujate deklareerimine, väärtuse omistamine ja kasutamine

---
+++ app.js
@@ -0,0 +1,18 @@
+ console.log("Tere Maailm")
+// üherealine kommentaar
+/* mitmerealine kommentaar heviii */
+
+// console.log('see ' + 'on ' + 'liitlause')
+
+ console.log("2016. aastal valiti Eesti presidendiks Kersti Kaljulaid")
+//muutujad ja andmetüübid
+ // 1.deklareerimine
+ // 2. väärtuse omistamine
+ // muutujanimi = väärtus
+
+ let aasta
+ aasta = 2016
+
+ let president
+ president = 'Kersti Kaljulaid'
+ console.log(aasta + '. Eesti presidendiks valiti ' + president)
