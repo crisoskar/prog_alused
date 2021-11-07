85de4006bb636ee27f956183a0bb65a9dbe40c5e
Cris <c.orav@khk.ee>
2021-11-07 18:19:25 +0200

    for tsükliga korduvate tegevuste sooritamine

---
+++ Tsüklid/Äratus.js
@@ -0,0 +1,6 @@
+// kasutaja sisend
+ let kordus = prompt('Mitu korda tuleb äratada?')
+// for tsükkel
+for (let kord = 1; kord <= kordus; kord++) {
+    console.log(kord + '. kord Tõuse ja sära');
+}
